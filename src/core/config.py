7f84c80a43d0711cc7f7b92bda65c0de7de0616f
Pavel Nikulenok <pashaaaa86@gmail.com>
2024-12-17 20:29:43 +0500

    fix

--- src/core/config.py
+++ src/core/config.py
@@ -3,4 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = f"postgresql+asyncpg://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASS')}@{os.environ.get('DB_HOST')}:{os.environ.get('DB_PORT')}/{os.environ.get('DB_NAME')}"
+DATABASE_URL = f"postgresql+asyncpg://{os.environ.get('DB_USER_FASTAPI')}:{os.environ.get('DB_PASSWORD_FASTAPI')}@{os.environ.get('DB_HOST')}:{os.environ.get('DB_PORT')}/{os.environ.get('DB_NAME_FASTAPI')}"
+
